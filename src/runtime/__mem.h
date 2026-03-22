93b12b47b4abd873d952778b5b640c253858429b
AP-NJA <noahjeremias31@icloud.com>
2026-03-22 18:57:35 -0400

    Create header for __mem.c

---
+++ src/runtime/__mem.h
@@ -0,0 +1,22 @@
+#ifndef RUNTIME_MEM_H
+#define RUNTIME_MEM_H
+
+#include "macros.h"
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    DECL_SECTION(".init")
+    void *memcpy(void *dest, const void *src, size_t count);
+    DECL_SECTION(".init")
+    void __fill_mem(void *dest, int val, size_t count);
+    DECL_SECTION(".init")
+    void *memset(void *dest, int ch, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
