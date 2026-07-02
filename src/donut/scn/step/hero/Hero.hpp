104057cc22c3dddeee7e4032899881610506f54d
AP-NJA <noahjeremias31@icloud.com>
2026-07-02 11:43:02 -0400

    Guard.cpp functions

--- src/donut/scn/step/hero/Hero.hpp
+++ src/donut/scn/step/hero/Hero.hpp
@@ -1,10 +1,16 @@
 #ifndef DONUT_SCN_STEP_HERO_HERO_HPP
 #define DONUT_SCN_STEP_HERO_HERO_HPP
 
+#include <types.h>
+
+#include "mem/ExplicitScopedPtr.hpp"
 #include "scn/step/common/Param.hpp"
 #include "scn/step/hero/Kind.hpp"
 
+/* clang-format off */
 namespace scn { namespace step { namespace hero {
+    class Guard; 
+
     class Param { // todo: put this in its own file
     public:
         common::Param* indiviKirby() const;
@@ -17,7 +23,12 @@ namespace scn { namespace step { namespace hero {
     public:
         Param* param() const;
         Kind kind() const; // code merged
+        Guard* guard();
+    private:
+        u8 m_0[0x168 - 0x0];
+        mem::ExplicitScopedPtr<Guard> mGuard;
     };
 }}}
+/* clang-format on */
 
 #endif
